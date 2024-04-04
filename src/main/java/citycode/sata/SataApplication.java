7b1dabbdc6f23dd623198d5d88f33729e379dc20
Lstortoni <89618746+Lstortoni@users.noreply.github.com>
2024-04-04 17:41:55 -0300

    Primer commit
    Es el primer commit

---
+++ src/main/java/citycode/sata/SataApplication.java
@@ -0,0 +1,13 @@
+package citycode.sata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/citycode/sata/SataApplicationTests.java
@@ -0,0 +1,13 @@
+package citycode.sata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
